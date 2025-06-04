0108b8ccbfd66c8d8faa3ffb48c359aacb7fccbe
Nestor <ness4b@gmail.com>
2025-06-04 16:24:04 +0200

    diseño v2 quitar bordes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6f8620f125e97e65"; }
+/******/ 		__webpack_require__.h = function() { return "01e52eeff33d876b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
