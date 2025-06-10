4dea23a43c296a98e0313a6f1d29b9a09fa5b2cf
Nestor <ness4b@gmail.com>
2025-06-10 19:28:19 +0200

    modificacion estilos pagina

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d76b9367ba5a4413"; }
+/******/ 		__webpack_require__.h = function() { return "0a0913a7327909a3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
