47eedee94c62463f4c8d6f7c1db8da5f1dd72be2
Nestor <ness4b@gmail.com>
2025-08-06 16:58:22 +0200

    sileuta escaner

--- components/CameraScanner.js
+++ components/CameraScanner.js
@@ -88,7 +88,7 @@ export default function CameraScanner({ onCapture, onClose }) {
       if (dbg)
         dbg.textContent = `Bordes: ${edgeCnt}\nDentro: ${inCnt}\nFuera: ${outCnt}\nFill%:  ${pct.toFixed(1)}`;
 
-      if (pct > 30 && edgeCnt > 150 && !doneRef.current) {
+      if (pct > 20 && edgeCnt > 100 && !doneRef.current) {
         doneRef.current = true;
         shoot();
         return;
